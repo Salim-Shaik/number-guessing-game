62a33acaf647f6259427b0376f981ee6d0f5e582
Salim-Shaik <salim7868786@gmail.com>
2022-03-15 22:48:57 +0200

    added game njoy

---
+++ import random.py
@@ -0,0 +1,16 @@
+import random
+
+def guess (y):
+    random_number = random.randint(1, y)
+    guess = 0 
+    while guess != random_number:
+        guess = int(input(f'GUess a number between 1 and {y}: '))
+        if guess< random_number:
+            print('TOo LOw')
+        elif guess > random_number:
+            print('tO hIgh')
+        
+
+    print('YOu did it, Congrats')
+ 
+guess(10)
